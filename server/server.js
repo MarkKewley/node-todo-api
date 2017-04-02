fe5144e8faaa00d7dc34cdd1f699e07ef09aa71f
Mark Kewley <markkewley@Mark-Kewleys-iMac.local>
2017-04-02 13:39:24 -0500

    Moves configuration of environments outside server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,4 @@
-const env = process.env.NODE_ENV || 'development';
-
-if (env === 'development') {
-    process.env.PORT = 9001;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 9001;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}
+require('./config/config');
 
 const _ = require('lodash');
 const express = require('express');
