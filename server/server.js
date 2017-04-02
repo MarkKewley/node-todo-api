d7f88216574e82aa8628961bd39e20580425e442
Mark Kewley <markkewley@Mark-Kewleys-iMac.local>
2017-04-02 11:36:54 -0500

    Adds GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -26,6 +27,21 @@ app.get('/todos', (req, res) => {
     Todo.find()
         .then(todos => res.send({ todos }))
         .catch(err => res.status(500).send(err))
+});
+
+app.get('/todos/:id', (req, res) => {
+    const { id } = req.params;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send(`Invalid Id: ${id}`)
+    }
+    Todo.findById(id)
+        .then(todo => {
+            if (!todo) {
+                return res.status(404).send('Unable to find todo', id);
+            }
+            res.send({ todo })
+        })
+        .catch(err => res.send(500))
 });
 
 app.listen(9001, () => {
